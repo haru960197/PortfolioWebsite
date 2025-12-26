ab78a12ea192489417b6d7a1da5c7ee19d8021bb
haru-lab-pc <ha.brack96@gmail.com>
2025-12-26 17:53:49 +0900

    :+1: Markdownのレンダーを変更

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,16 @@
 // @ts-check
 import tailwindcss from '@tailwindcss/vite';
 import { defineConfig } from 'astro/config';
+import remarkToc from 'remark-toc';
+import { rehypeAccessibleEmojis } from 'rehype-accessible-emojis';
 
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
+  },
+  markdown: {
+    remarkPlugins: [ [remarkToc, { heading: 'toc', maxDepth: 3 } ] ],
+    // rehypePlugins: [rehypeAccessibleEmojis],
   },
 });
